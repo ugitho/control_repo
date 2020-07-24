3d2446194e24706744fda5db0236df5375f3417b
NGUYEN KHAC <tho.nguyenkhac1325@gmail.com>
2020-07-24 23:33:31 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
           ensure  => present,
           user    => 'root',
           type    => 'ssh-rsa',
-          key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBuzy02cEWZbAQ79pXjjHHi3mT3cqsXRB5E0efHDfUEBKjuaYiCb8SL7lWm1/8V5hiPEmeJB45rcilC1fhyWfNEJGIeX1ptmAApdwX2oERJK2NqHB1Fo3FzF+b9t4M3kI1npCzvV0dXtMhrlk8dGw2h0mbdAQNviN/RjhpUhW8jn7uDNTzvXeakfyUIJPxv6uJcB8AnZnJXJpsJFy0LEk9KliaNYFhgy8og0ldG25Kf5+AM+TEd4NhMrhHMMTG1RP3nRW1iQTEe8O2CEyAsKaAhEZkhX2AtetXigqvLewlX3G8/rIqhV6FEWmB3UO2m51y8pK6r01CXdBD5U1TDK/r root@master.puppet.vm',
+          key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBuzy02cEWZbAQ79pXjjHHi3mT3cqsXRB5E0efHDfUEBKjuaYiCb8SL7lWm1/8V5hiPEmeJB45rcilC1fhyWfNEJGIeX1ptmAApdwX2oERJK2NqHB1Fo3FzF+b9t4M3kI1npCzvV0dXtMhrlk8dGw2h0mbdAQNviN/RjhpUhW8jn7uDNTzvXeakfyUIJPxv6uJcB8AnZnJXJpsJFy0LEk9KliaNYFhgy8og0ldG25Kf5+AM+TEd4NhMrhHMMTG1RP3nRW1iQTEe8O2CEyAsKaAhEZkhX2AtetXigqvLewlX3G8/rIqhV6FEWmB3UO2m51y8pK6r01CXdBD5U1TDK/r',
   }
 }
