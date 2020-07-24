823699893b31f427705fb911701b46e55343b74d
NGUYEN KHAC <tho.nguyenkhac1325@gmail.com>
2020-07-24 22:16:38 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a README',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner   => 'root',
-  }
 }
