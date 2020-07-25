67f5aad684a8fb113d11b31eb7faf08d98c4e487
NGUYEN KHAC <tho.nguyenkhac1325@gmail.com>
2020-07-25 13:44:11 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,18 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar'
+  $install_dir = '/opt/minecraft'
+  file {$install_dir:
     ensure  => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar":
     ensure  => file,
-    source  => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+    source  => $url ,
+    before  => Service['minecraft'],
   }
   package {'java':
     ensure  => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
@@ -20,5 +23,6 @@ class minecraft {
   service {'minecraft':
     ensure  => running,
     enable  => true,
+    require => [Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']],
   }
 }
