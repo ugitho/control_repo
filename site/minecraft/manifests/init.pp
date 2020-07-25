83c21d9aab874d83394f5f23d1357021fbe967ad
NGUYEN KHAC <tho.nguyenkhac1325@gmail.com>
2020-07-25 12:15:29 +0200

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure  => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensire  => file,
+    source  => 'https://s3amazonaws.com/Minecraft.Download/version/1.16.1/minecraft_server.1.16.1.jar',
+  }
+  package {'java':
+    ensure  => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    content => 'eula=true',
+  }
+  file{'/etc/systemd/system/minecraft.service':
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  
+}
